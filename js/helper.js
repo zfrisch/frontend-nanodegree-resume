64d49e202031311ea3fadf14058e6a734c291b96
zfrisch <zfrisch@gmail.com>
2016-02-01 22:15:36 -0600

    "resume completed"

--- js/helper.js
+++ js/helper.js
@@ -64,8 +64,10 @@ The International Name challenge in Lesson 2 where you'll create a function that
 */
 $(document).ready(function() {
   $('button').click(function() {
-    var iName = inName() || function(){};
+	var resumeName = $("#name").text();
+    var iName = inName(resumeName) || function(){};
     $('#name').html(iName);  
+	
   });
 });
 
@@ -85,7 +87,9 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+  var x = loc.pageX;
+  var y = loc.pageY;
+  logClicks(x,y);
 });
 
 
@@ -124,10 +128,8 @@ function initializeMap() {
 
     // initializes an empty array
     var locations = [];
-
     // adds the single location property from bio to the locations array
-    locations.push(bio.contacts.location);
-
+    locations.push(bio.contact.location);
     // iterates through school locations and appends each location to
     // the locations array. Note that forEach is used for array iteration
     // as described in the Udacity FEND Style Guide: 
@@ -135,7 +137,6 @@ function initializeMap() {
     education.schools.forEach(function(school){
       locations.push(school.location);
     });
-
     // iterates through work locations and appends each location to
     // the locations array. Note that forEach is used for array iteration
     // as described in the Udacity FEND Style Guide: 
@@ -143,10 +144,8 @@ function initializeMap() {
     work.jobs.forEach(function(job){
       locations.push(job.location);
     });
-
     return locations;
   }
-
   /*
   createMapMarker(placeData) reads Google Places search results to create map pins.
   placeData is the object returned from search results containing information
@@ -176,7 +175,7 @@ function initializeMap() {
 
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
+      infoWindow.open(map, marker);
     });
 
     // this is where the pin actually gets added to the map.
@@ -238,11 +237,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,181 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+
+var bio = {
+	"name": "Zak Frisch",
+	"role": "Web Developer",
+	"contact": {
+			"email": "zfrisch@gmail.com",
+			"phone": "920-660-1089",
+			"github": "zfrisch",
+			"location": "Green Bay"
+	},
+	"pic": "http://vignette3.wikia.nocookie.net/es.futurama/images/4/4f/Fry.JPG/revision/latest?cb=20110605191102",
+	"welcome": "Welcome to my Resume!",
+	"skills": ['HTML 5', 'CSS3', 'JavaScript', 'jQuery', 'Node.js', 'BootStrap', 'Foundation'],
+	"display": function() {
+		var formattedName = HTMLheaderName.replace("%data%", bio.name);
+		var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+		var formattedMobile = HTMLmobile.replace('%data%', bio.contact.phone);
+		var formattedGithub = HTMLgithub.replace('%data%', bio.contact.github);
+		var formattedEmail = HTMLemail.replace('%data%', bio.contact.email);
+		var formattedLocation = HTMLlocation.replace('%data%', bio.contact.location);
+		var formattedPic = HTMLbioPic.replace('%data%', bio.pic);
+		var formattedWelcome = HTMLwelcomeMsg.replace('%data%', bio.welcome);
+	
+		
+		$("#header").prepend(formattedRole).prepend(formattedName);
+		$("#topContacts, #footerContacts").append(formattedMobile);
+		$("#topContacts, #footerContacts").append(formattedEmail);
+		$("#topContacts, #footerContacts").append(formattedGithub);
+		$("#topContacts, #footerContacts").append(formattedLocation);
+		$("#header").append(formattedPic);
+		$("#header").append(formattedWelcome);
+		$("#header").append(HTMLskillsStart);
+			for(i=0;i<bio.skills.length;i++) {
+			var skill=	HTMLskills.replace('%data%', bio.skills[i]);
+			$("#skills").append(skill);
+			};
+		}
+};
+var education = {
+		"schools": [ 
+		{
+		"name": "NWTC",
+		"location": "Green Bay, WI",
+		"degree":"Bachelor of Technical Sciences",
+		"major": ["Networking"],
+		"dates":"2014-2015",
+		"url": "https://www.nwtc.edu/"
+		},
+		{
+		"name": "Florida Mahatma University",
+		"location": "Tampa, FL",
+		"degree":"Extra-marital Arts",
+		"major": ["I majored in having a useless degree"],
+		"dates":"2010-2013",
+		"url": "https://www.florida.com/"
+		}
+		],
+		"onlineCourses": [
+		{
+		"title": "Front-End Web Developer NanoDegree",
+		"school":"Udacity",
+		"dates":"Jan 2016 - Mar 2016",
+		"url":"http://www.udacity.com/"
+		}
+		],
+		"display": function() {
+		$("#education").append(HTMLschoolStart);
+		for(item in education.schools) {
+		var formattedschoolName = HTMLschoolName.replace('%data%', education.schools[item].name);
+		var formattedschoolDegree = HTMLschoolDegree.replace('%data%', education.schools[item].degree);
+		var formattedschoolDates = HTMLschoolDates.replace('%data%', education.schools[item].dates);
+		var formattedschoolLocation = HTMLschoolLocation.replace('%data%', education.schools[item].location);
+		var allmajors="";
+		for(i=0; i<education.schools[item].major.length;i++) {
+		var formattedschoolMajor = HTMLschoolMajor.replace('%data%', education.schools[item].major[i]);	
+		allmajors = allmajors + formattedschoolMajor;
+		}
+		$('.education-entry:last').append(formattedschoolName)
+		.append(formattedschoolDegree)
+		.append(formattedschoolDates)
+		.append(formattedschoolLocation)
+		.append(allmajors);
+		};	
+		$("#education").append(HTMLonlineClasses);
+		$("#education").append(HTMLschoolStart);
+		for(item in education.onlineCourses) {
+		var formattedonlinetitle = HTMLonlineTitle.replace('%data%', education.onlineCourses[item].title);
+		var formattedonlineschool = HTMLonlineSchool.replace('%data%',education.onlineCourses[item].school);
+		var formattedonlinedates = HTMLonlineDates.replace('%data%', education.onlineCourses[item].dates);
+		var formattedonlineurl = HTMLonlineURL.replace('%data%', education.onlineCourses[item].url);
+		$(".education-entry:last").append(formattedonlinetitle)
+		.append(formattedonlineschool)
+		.append(formattedonlinedates)
+		.append(formattedonlineurl);
+		}
+	}
+};
+	
+var work = {	
+		"jobs": [
+		{
+		"employer":"Nsight/Cellcom",
+		"title":"Interactive Equipment Guide Developer",
+		"dates":"January 2013 - Present",
+		"description": "Work as Developer for an Interactive Guide. Job requires HTML, CSS, JavaScript, jQuery, PHP, and Microsoft Office proficiency. I also consult other departments regarding technical matters and assist with out of scope issues as they arise.",
+		"location": "De Pere, WI"
+		},
+		{
+		"employer":"Bob's Burgers",
+		"title":"Burger Flipping Malcontent",
+		"dates":"January 2010 - 2013",
+		"description": "Made Burgers. Constantly disputed wages with Employer, Bob, and fought off his three insane children and nagging wife.",
+		"location": "Baraboo, WI"
+		}
+		],
+		"display": function() { 
+		for(item in work.jobs) {
+		$("#workExperience").append(HTMLworkStart);
+		var formatworkEmployer = HTMLworkEmployer.replace('%data%',work.jobs[item].employer);
+		var formatworkTitle = HTMLworkTitle.replace('%data%',work.jobs[item].title);
+		var formatworkDates = HTMLworkDates.replace('%data%',work.jobs[item].dates);
+		var formatworkLocation = HTMLworkLocation.replace('%data%',work.jobs[item].location);
+		var formatworkDescription = HTMLworkDescription.replace('%data%',work.jobs[item].description);
+		$(".work-entry:last")
+		.append(formatworkEmployer)
+		.append(formatworkTitle)
+		.append(formatworkDates)
+		.append(formatworkLocation)
+		.append(formatworkDescription);
+		};
+		}
+};
+var projects = {
+	"projects": [
+	{
+	"title": "Salarium", 
+	"dates": "Jan 2016",
+	"description": "Node.js Module and Grunt Plugin to allow for easily created, responsive images.",
+	"images": ["http://i.stack.imgur.com/y2NfZ.png"]
+	},
+	{
+	"title": "Resume", 
+	"dates": "January 2016",
+	"description": "Resume Project From Udacity!",
+	"images": ["http://10poundtees.co.uk/image/cache//data/designs/dont_panic/DontPanic_t_shirt_tn-300x300.jpg"]
+	}
+	],
+	"display": function() {
+		for (item in projects.projects) {
+			$('#projects').append(HTMLprojectStart);
+			var formattedprojecttitle = HTMLprojectTitle.replace('%data%', projects.projects[item].title);
+			var formattedprojectdates = HTMLprojectDates.replace('%data%', projects.projects[item].dates);
+			var formattedprojectdescription = HTMLprojectDescription.replace('%data%', projects.projects[item].description);
+			var allimages = "";
+			for(i=0;i < projects.projects[item].images.length;i++) {
+				var formattedprojectimage = HTMLprojectImage.replace('%data%', projects.projects[item].images[i]);
+				allimages = allimages + formattedprojectimage;
+			}
+			$(".project-entry:last").append(formattedprojecttitle)
+			.append(formattedprojectdates)
+			.append(formattedprojectdescription)
+			.append(allimages);
+		};
+	}
+}
+function inName(string) {
+var nameArray = string.toLowerCase().split(" ");
+nameArray[0] = nameArray[0].charAt(0).toUpperCase() + nameArray[0].slice(1);
+nameArray[1] = nameArray[1].toUpperCase();
+string = nameArray[0] + " " + nameArray[1];
+
+return string;
+}
+
+$("#main").append(internationalizeButton);
+$("#mapDiv").append(googleMap);
+
+
+
+
+
